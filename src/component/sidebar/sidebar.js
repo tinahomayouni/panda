8a674c677b6112ec0927cf79451f954928436359
Tina <tinahomayouni@gmail.com>
2020-07-22 19:26:21 +0430

    log select value

--- src/component/sidebar/sidebar.js
+++ src/component/sidebar/sidebar.js
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col } from 'react-bootstrap';
 import data from '../../data/dataCard/dataCard.json';
+import "./sidebar.scss"
 
 function Sidebar() {
+  const [options, setOptions] = useState('')
+
+
   let uniquFilter = [];
+  let category = [];
   data.houses.forEach(house => {
     house.option.forEach(opt => {
       const key = Object.keys(opt)[0];
       if (!uniquFilter.includes(key)) {
         uniquFilter.push(key);
       }
+      if (!category.includes(house.category)) {
+        category.push(house.category)
+      }
     });
-  });
-  function selector() {
-    alert('changed!!');
-  }
 
+  });
+
+  function doFilter() {
+    console.log(options)
+
+  }
   return (
     <Col xs={4}>
       <aside>
-        <select onChange={selector}>
+        <label>Option:</label>
+        <select id="options" onChange={(e) => setOptions(e.target.value)}>
           <option>Choose!</option>
           {uniquFilter.map((opt, index) => {
             return <option key={index}>{opt}</option>;
           })}
         </select>
+
+        <label>Category:</label>
+        <select>
+          <option>Choose!</option>
+          {category.map(gt => {
+            return <option>{gt}</option>
+          })}
+        </select>
+
+        <button onClick={doFilter}>Filter</button>
       </aside>
     </Col>
   );
